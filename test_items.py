298dfcc74edef3a8d2a569c44d8879324e1a8679
AEpolyakov <ae.polyakov@mail.ru>
2021-11-07 21:36:38 +0400

    assert message

--- test_items.py
+++ test_items.py
@@ -5,11 +5,11 @@ from selenium.webdriver.common.by import By
 
 def test_button_to_basket(browser):
     browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
-    time.sleep(30)
+    time.sleep(3)
 
     button_to_basket = browser.find_element(By.XPATH, '//form[@id="add_to_basket_form"]/button')
 
-    assert button_to_basket
+    assert button_to_basket, 'отсутствует кнопка "В корзину"'
 
 
 if __name__ == "__main__":
